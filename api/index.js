b05c4f06092f423836fcae0e0c6ef08f8b2be866
esakkivenkatesh <esakkivenkatesh6@gmail.com>
2026-08-18 22:20:28 +0530

    fix(api): explicit req/res handling for vercel serverless function

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
 const app = require('../backend/server');
 
-module.exports = app;
+module.exports = (req, res) => {
+  if (!process.env.JAMENDO_CLIENT_ID) {
+    console.warn('[VibeStream-Vercel] JAMENDO_CLIENT_ID is not set in process.env!');
+  }
+  return app(req, res);
+};
